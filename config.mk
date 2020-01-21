63cd9d79bb868b801b9283c8339cee81e88f8ee5
Travis West <travis@traviswest.ca>
2020-01-20 21:20:37 -0500

    initial config.mk tweak

--- config.mk
+++ config.mk
@@ -5,10 +5,10 @@ VERSION = 0.8.2
 
 # paths
 PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+MANPREFIX = /usr/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
 
 PKG_CONFIG = pkg-config
 
